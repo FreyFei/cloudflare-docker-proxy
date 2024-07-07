c51c2b2022ea34285d84ec80074e0129752922ec
Frey <24317147@qq.com>
2024-07-07 23:11:15 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,14 +7,14 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.lowyun.com": dockerHub,
+  "quay.lowyun.com": "https://quay.io",
+  "gcr.lowyun.com": "https://gcr.io",
+  "k8s-gcr.lowyun.com": "https://k8s.gcr.io",
+  "k8s.lowyun.com": "https://registry.k8s.io",
+  "ghcr.lowyun.com": "https://ghcr.io",
+  "cloudsmith.lowyun.com": "https://docker.cloudsmith.io",
+  "ecr.lowyun.com": "https://public.ecr.aws",
 
   // staging
   "docker-staging.libcuda.so": dockerHub,
